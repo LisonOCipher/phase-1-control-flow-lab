ccfd4169d1061d57cc0a241167643bf6925fe492
Danson Lino <danson.lino@student.moringaschool.com>
2023-10-17 02:42:46 +0300

    let's commit

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 function scuberGreetingForFeet(){
-  // Write your code here!
-}
+  let scuberGreetingForFeet = 199;
+  if (scuberGreetingForFeet <= 400) {
+    return("This one is on me!");
+  }
+  else(scuberGreetingForFeet >= 2000); {
+    return ("I will gladly take your thirty bucks.");
+  } 
+  elseif(scuberGreetingForFeet >= 2500); {
+    return("No can do")
+  }
+};
 
 function ternaryCheckCity(){
-  // Write your code here!
-}
+  let ternaryCheckCity = "NYC";
+  if(ternaryCheckCity = "NYC"){
+    return("Ok, sounds good.")
+  }
+  else(ternaryCheckCity = "Pittsburgh") => {
+    return("No go.")
+  }
+};
 
 function switchOnCharmFromTip(){
-  // Write your code here!
+  let switchOnCharmFromTip = "Generous";
+if(switchOnCharmFromTip = "generous"){
+  return("thank you so much.")
 }
+else(switchOnCharmFromTip = "not very generous")=>{
+  return("thank you.")
+}
+elseif(switchOnCharmFromTip = "thanks for everything");{
+  return("bye.")
+}         
+};
